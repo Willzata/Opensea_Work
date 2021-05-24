26ea77fa159cb97c860ae8b013afd6dc0097d968
Will Mangion <54841253+Willzata@users.noreply.github.com>
2021-05-24 16:03:44 +0100

    Update Create_Database_Tables.py

--- Create_Database_Tables.py
+++ Create_Database_Tables.py
@@ -1,49 +1,17 @@
-#The API can return the same image twice, 
 #Create the database
 
 import sqlite3
 
-path = 'db_path'
+
+path = '/mnt/c/Users/wmang/Python Stuff/Opensea_2/database/assets.db'
+
 conn = sqlite3.connect(path)
 print('Established Connection')
 #Creating a cursor object using the cursor() method
 cursor = conn.cursor()
 
-#Doping Assets table if already exists.
+#Dropping Assets table if already exists.
 cursor.execute("DROP TABLE IF EXISTS Assets")
-
-#Creating table as per requirement
-sql = '''CREATE TABLE Assets (
-   id integer PRIMARY KEY,
-   token_id integer,
-   image_URL text,
-   auction_URL text,
-   asset_contract_type text,
-   art_name text,
-   artist_name text,
-   twitter text
-   #filepath text
-)'''
-
-cursor.execute(sql)
-print("Table created successfully........")
-
-# Commit your changes in the database
-conn.commit()
-
-#Closing the connection
-conn.close()
-
-
-#Create the second database table, containing the filepath 
-path = 'db_path'
-conn = sqlite3.connect(path)
-print('Established Connection')
-#Creating a cursor object using the cursor() method
-cursor = conn.cursor()
-
-#Doping Assets_full table if already exists.
-cursor.execute("DROP TABLE IF EXISTS Assets_full")
 
 #Creating table as per requirement
 sql = '''CREATE TABLE Assets (
